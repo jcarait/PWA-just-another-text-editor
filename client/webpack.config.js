2feb9d594ae86586f4638c879314771a55438904
Jonathan Florido-Carait <jonathan.carait@outlook.com>
2022-04-17 21:04:37 +1000

    commented out service worker due to erros when running in dev mode

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,10 +25,10 @@ module.exports = () => {
       }),
 
       // Injects our custom service worker
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js'
-      }),
+      // new InjectManifest({
+      //   swSrc: './src-sw.js',
+      //   swDest: 'src-sw.js'
+      // }),
       
     ],
 
