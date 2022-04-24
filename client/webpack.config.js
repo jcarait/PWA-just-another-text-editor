56337d338d89c569ce92dd516147c261edc15f7a
Jonathan Florido-Carait <jonathan.carait@outlook.com>
2022-04-24 19:55:59 +1000

    uncommented custom service worker in webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -24,12 +24,13 @@ module.exports = () => {
         title: 'Text Editor'
       }),
 
-      // Injects our custom service worker
-      // new InjectManifest({
-      //   swSrc: './src-sw.js',
-      //   swDest: 'src-sw.js'
-      // }),
+      //Injects our custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
       
+      // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
