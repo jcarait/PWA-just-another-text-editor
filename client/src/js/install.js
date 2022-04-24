49fc9674247bbe07c4204961e1dfcbe6cbbbba8a
Jonathan Florido-Carait <jonathan.carait@outlook.com>
2022-04-24 19:49:24 +1000

    added comments to explain logic for installing PWA

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,7 +2,11 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 window.addEventListener('beforeinstallprompt', (event) => {
+
+    // Store the triggered events
     window.deferredPrompt = event
+
+    // Remove the hidden class from the button.
     butInstall.classList.toggle("hidden", false);
 });
 
@@ -12,11 +16,16 @@ butInstall.addEventListener('click', async () => {
     if (!promptEvent) {
         return;
     }
+
+    // Show prompt
     promptEvent.prompt();
+
+    // Reset the deferred prompt variable, it can only be used once.
     window.deferredPrompt = null;
     butInstall.classList.toggle("hidden", true);
 });
 
 window.addEventListener('appinstalled', (event) => {
+    // Clear prompt
     window.deferredPrompt = null;
 });
